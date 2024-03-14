e42cfaf63bbdb92a7a3e7a9dca9ed863cc3d3c9a
SupremeDarkman <64687911+SupremeDarkman@users.noreply.github.com>
2024-03-14 11:32:36 +0200

    Update main.cpp

--- WarWithCards/main.cpp
+++ WarWithCards/main.cpp
@@ -1,5 +1,7 @@
 #include "Manager.h"
 #include <SDL.h>
+#include "App.h"
+#include <iostream>
 
 App* app = nullptr;
 const unsigned WINDOW_WIDTH = 800;
@@ -7,36 +9,39 @@ const unsigned WINDOW_HEIGHT = 600;
 
 int main(int argc, char** argv)
 {
+	Deck deck;
 	Player player1, player2, player3;
+    std::vector<Player> players;
+    
+    players.push_back(player1);
+    players.push_back(player2);
+    players.push_back(player3);
 
-	app = new App(player1, player2, player3);
+	deck.shuffle();
+	deck.riffleShuffle();
 
+    Manager manager(player1, player2, player3);
+    
+    app = new App(players);
+    bool init = app->init("WarWithCards",
+        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+        WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_RESIZABLE);
+    app->loadTextureOnDeck(deck);
 
-	if (!app->init("WarWithCards",
-		SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_RESIZABLE
-	))
+	if (!init)
 	{
 		return -1;
 	}
 
-	Deck deck;
-	deck.shuffle();
-
-	player1.dealCards(deck);
-	player2.dealCards(deck);
-	player3.dealCards(deck);
-
-
-
-
-
 	app->ttf_init();
 	while (app->isRunning())
 	{
-		app->handleEvents();
+		app->handleEvents(deck);
 		app->update();
 		app->render();
 	}
+    app->DestroySDL();
+
 
 	/*
 	Deck deck;
